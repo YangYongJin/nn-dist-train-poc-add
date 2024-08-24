3b39b36e0833da0d33fd926103e9ac0e3a847d60
gartland98 <gartland223@gmail.com>
2024-08-24 18:13:20 +0900

    flplus

--- fedcon_plus_reid/utils.py
+++ fedcon_plus_reid/utils.py
@@ -647,6 +647,7 @@ def test(
     print('Rank@10:%f mAP:%f'%(acc, ap))
     print('-'*15)
     print()
+    wandb.log({"rank@10": acc, f"mAP": ap})
     return CMC, ap, gallery_loss, acc
     #print(self.model_name)
     #print(dataset):
