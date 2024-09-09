0cc6c2da7f81e8f835bb806957f4397856b32e77
gartland98 <gartland223@gmail.com>
2024-09-09 21:39:57 +0900

    add feddr+

---
+++ feddr+_embedded_devices/client.py
@@ -0,0 +1,231 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Flower client example using PyTorch for CIFAR-10 image classification."""
+
+
+import argparse
+import timeit
+from collections import OrderedDict
+from importlib import import_module
+
+import flwr as fl
+import numpy as np
+import torch
+import torchvision
+from flwr.common import EvaluateIns, EvaluateRes, FitIns, FitRes, ParametersRes, Weights
+
+from torch.utils.data import Dataset
+import random
+
+import utils
+
+
+class DatasetSplit(Dataset):
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = idxs
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return image, label
+
+# pylint: disable=no-member
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# pylint: enable=no-member
+
+
+def get_weights(model: torch.nn.ModuleList) -> fl.common.Weights:
+    """Get model weights as a list of NumPy ndarrays."""
+    return [val.cpu().numpy() for _, val in model.state_dict().items()]
+
+
+def set_weights(model: torch.nn.ModuleList, weights: fl.common.Weights) -> None:
+    """Set model weights from a list of NumPy ndarrays."""
+    state_dict = OrderedDict(
+        {
+            k: torch.tensor(np.atleast_1d(v))
+            for k, v in zip(model.state_dict().keys(), weights)
+        }
+    )
+    model.load_state_dict(state_dict, strict=True)
+
+
+class CifarClient(fl.client.Client):
+    """Flower client implementing CIFAR-10 image classification using
+    PyTorch."""
+
+    def __init__(
+        self,
+        cid: str,
+        model: torch.nn.Module,
+        trainset: torchvision.datasets.CIFAR10,
+        testset: torchvision.datasets.CIFAR10,
+        classes: list,
+    ) -> None:
+        self.cid = cid
+        self.model = model
+        self.trainset = trainset
+        self.testset = testset
+        self.classes = classes
+
+        # make random lists for selection of classes
+        # select 80% for classes in self.classes and 20% for other classes
+        self.id_idxs = []
+        for i, (_, label) in enumerate(self.trainset):
+            if label in self.classes:
+                if random.random() < 0.8:
+                    self.id_idxs.append(i)
+            else:
+                if random.random() < 0.2:
+                    self.id_idxs.append(i)
+
+
+    def get_parameters(self) -> ParametersRes:
+        print(f"Client {self.cid}: get_parameters")
+
+        weights: Weights = get_weights(self.model)
+        parameters = fl.common.weights_to_parameters(weights)
+        return ParametersRes(parameters=parameters)
+
+    def _instantiate_model(self, model_str: str):
+
+        # will load utils.model_str
+        m = getattr(import_module("utils"), model_str)
+        # instantiate model
+        self.model = m()
+
+    def fit(self, ins: FitIns) -> FitRes:
+        print(f"Client {self.cid}: fit")
+
+        weights: Weights = fl.common.parameters_to_weights(ins.parameters)
+        config = ins.config
+        fit_begin = timeit.default_timer()
+
+        # Get training config
+        epochs = int(config["epochs"])
+        lr = float(config["lr"])
+        batch_size = int(config["batch_size"])
+        optimizer_name = config["optimizer"]
+        pin_memory = bool(config["pin_memory"])
+        num_workers = int(config["num_workers"])
+
+        # Set model parameters
+        set_weights(self.model, weights)
+
+        if torch.cuda.is_available():
+            kwargs = {
+                "num_workers": num_workers,
+                "pin_memory": pin_memory,
+                "drop_last": True,
+            }
+        else:
+            kwargs = {"drop_last": True}
+
+        # Train model
+
+        trainloader = torch.utils.data.DataLoader(
+            DatasetSplit(self.trainset, self.id_idxs), batch_size=batch_size, shuffle=True, **kwargs
+        )
+        utils.train(self.model, trainloader, lr=lr, epochs=epochs, optimizer_n=optimizer_name, device=DEVICE)
+
+        # Return the refined weights and the number of examples used for training
+        weights_prime: Weights = get_weights(self.model)
+        params_prime = fl.common.weights_to_parameters(weights_prime)
+        num_examples_train = len(self.trainset)
+        metrics = {"duration": timeit.default_timer() - fit_begin}
+        return FitRes(
+            parameters=params_prime, num_examples=num_examples_train, metrics=metrics
+        )
+
+    def evaluate(self, ins: EvaluateIns) -> EvaluateRes:
+        print(f"Client {self.cid}: evaluate")
+
+        weights = fl.common.parameters_to_weights(ins.parameters)
+
+        # Use provided weights to update the local model
+        set_weights(self.model, weights)
+
+        # Evaluate the updated model on the local dataset
+        testloader = torch.utils.data.DataLoader(
+            self.testset, batch_size=32, shuffle=False
+        )
+        loss, accuracy = utils.test(self.model, testloader, device=DEVICE)
+
+        # Return the number of evaluation examples and the evaluation result (loss)
+        metrics = {"accuracy": float(accuracy)}
+        return EvaluateRes(
+            loss=float(loss), num_examples=len(self.testset), metrics=metrics
+        )
+
+
+def main() -> None:
+    """Load data, create and start CifarClient."""
+    parser = argparse.ArgumentParser(description="Flower")
+    parser.add_argument(
+        "--server_address",
+        type=str,
+        required=True,
+        help=f"gRPC server address",
+    )
+    parser.add_argument(
+        "--cid", type=str, required=True, help="Client CID (no default)"
+    )
+    parser.add_argument(
+        "--log_host",
+        type=str,
+        help="Logserver address (no default)",
+    )
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        help="Directory where the dataset lives",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="ResNet18",
+        choices=["Net", "ResNet18","ResNet8"],
+        help="model to train",
+    )
+    # arguments for self.classes (default: [0,1]) list
+    parser.add_argument(
+        "--classes",
+        nargs="+",
+        type=int,
+        default=[0,1],
+        help="list of classes to train on",
+    )
+    
+    args = parser.parse_args()
+
+    # Configure logger
+    fl.common.logger.configure(f"client_{args.cid}", host=args.log_host)
+
+    # model
+    model = utils.load_model(args.model)
+    model.to(DEVICE)
+    # load (local, on-device) dataset
+    trainset, testset = utils.load_cifar()
+
+    # Start client
+    client = CifarClient(args.cid, model, trainset, testset, args.classes)
+    fl.client.start_client(args.server_address, client)
+
+
+if __name__ == "__main__":
+    main()
---
+++ feddr+_embedded_devices/server.py
@@ -0,0 +1,248 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Minimal example on how to start a simple Flower server."""
+
+
+import argparse
+from collections import OrderedDict
+from typing import Callable, Dict, Optional, Tuple
+from logging import INFO, DEBUG
+import flwr as fl
+import numpy as np
+import torch
+import torchvision
+import random
+import utils
+import time
+import os
+import wandb
+
+torch.backends.cudnn.deterministic=True
+torch.backends.cudnn.benchmark=False
+
+
+# pylint: disable=no-member
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# pylint: enable=no-member
+
+parser = argparse.ArgumentParser(description="Flower")
+
+# set seed
+parser.add_argument(
+    "--random_seed",
+    type=int,
+    default=1,
+    help="random seed (default: 1)",
+)
+
+parser.add_argument(
+    "--server_address",
+    type=str,
+    required=True,
+    help=f"gRPC server address",
+)
+parser.add_argument(
+    "--rounds",
+    type=int,
+    default=1,
+    help="Number of rounds of federated learning (default: 1)",
+)
+parser.add_argument(
+    "--sample_fraction",
+    type=float,
+    default=1.0,
+    help="Fraction of available clients used for fit/evaluate (default: 1.0)",
+)
+parser.add_argument(
+    "--min_sample_size",
+    type=int,
+    default=2,
+    help="Minimum number of clients used for fit/evaluate (default: 2)",
+)
+parser.add_argument(
+    "--min_num_clients",
+    type=int,
+    default=2,
+    help="Minimum number of available clients required for sampling (default: 2)",
+)
+parser.add_argument(
+    "--log_host",
+    default="./log",
+    type=str,
+    help="Log directory (no default)",
+)
+parser.add_argument(
+    "--model",
+    type=str,
+    default="ResNet18",
+    choices=["Net", "ResNet18","ResNet8"],
+    help="model to train",
+)
+parser.add_argument(
+    "--batch_size",
+    type=int,
+    default=32,
+    help="training batch size",
+)
+
+parser.add_argument(
+    "--epochs",
+    type=int,
+    default=1,
+    help="number of epochs to train",
+)
+
+parser.add_argument(
+    "--lr",
+    type=float,
+    default=0.1,
+    help="learning rate",
+)
+
+parser.add_argument(
+    "--optimizer",
+    type=str,
+    default="SGD",
+    choices=["SGD", "Adam", "RMSprop", "SGDM"],
+    help="optimizer to use",
+)
+
+parser.add_argument(
+    "--num_workers",
+    type=int,
+    default=4,
+    help="number of workers for dataset reading",
+)
+parser.add_argument("--pin_memory", action="store_true")
+args = parser.parse_args()
+
+BEST_ACCURACY = 0.0
+
+def main() -> None:
+    """init wandb"""
+    wandb.init(project="feddr+")
+    wandb.config.update(args)
+
+    """Start server and train five rounds."""
+
+    print(args)
+    torch.manual_seed(args.random_seed)
+    torch.cuda.manual_seed(args.random_seed)
+    np.random.seed(args.random_seed)
+    random.seed(args.random_seed)
+
+
+
+    assert (
+        args.min_sample_size <= args.min_num_clients
+    ), f"Num_clients shouldn't be lower than min_sample_size"
+
+    # Configure logger
+    fl.common.logger.configure(identifier=f"FedFN_{args.random_seed}", filename=os.path.join(args.log_host, f"seed_{args.random_seed}.log"))
+
+    # Load evaluation data
+    _, testset = utils.load_cifar(download=True)
+
+    # Create client_manager, strategy, and server
+    client_manager = fl.server.SimpleClientManager()
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=args.sample_fraction,
+        min_fit_clients=args.min_sample_size,
+        min_available_clients=args.min_num_clients,
+        eval_fn=get_eval_fn(testset),
+        on_fit_config_fn=fit_config,
+    )
+    server = fl.server.Server(client_manager=client_manager, strategy=strategy)
+
+    # Start timer
+    start_time = time.time()
+
+    # Run server
+    fl.server.start_server(
+        server_address=args.server_address,
+        server=server,
+        config={"num_rounds": args.rounds},
+    )
+
+    # Stop timer in minutes
+    end_time = time.time()
+    print(f"Total time for training {args.rounds}: {(end_time - start_time)/60} minutes")
+
+    # Best Performance
+    
+
+
+def fit_config(server_round: int) -> Dict[str, fl.common.Scalar]:
+    """Return a configuration with static batch size and (local) epochs."""
+    config = {
+        "epoch_global": str(server_round),
+        "epochs": str(args.epochs),
+        "lr": str(args.lr),
+        "batch_size": str(args.batch_size),
+        "num_workers": str(args.num_workers),
+        "pin_memory": str(args.pin_memory),
+        "optimizer": str(args.optimizer),
+    }
+    return config
+
+
+def set_weights(model: torch.nn.ModuleList, weights: fl.common.Weights) -> None:
+    """Set model weights from a list of NumPy ndarrays."""
+    state_dict = OrderedDict(
+        {
+            k: torch.tensor(np.atleast_1d(v))
+            for k, v in zip(model.state_dict().keys(), weights)
+        }
+    )
+    model.load_state_dict(state_dict, strict=True)
+
+
+def get_eval_fn(
+    testset: torchvision.datasets.CIFAR10,
+) -> Callable[[fl.common.Weights], Optional[Tuple[float, float]]]:
+    """Return an evaluation function for centralized evaluation."""
+
+    def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+        """Use the entire CIFAR-10 test set for evaluation."""
+
+        model = utils.load_model(args.model)
+        set_weights(model, weights)
+        model.to(DEVICE)
+
+        testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+        loss, accuracy = utils.test(model, testloader, device=DEVICE)
+
+        # log this accuracy
+        if args.log_host:
+            fl.common.logger.log(
+                INFO, f"eval/loss {loss} accuracy {accuracy}"
+            )
+        wandb.log({"accuracy": accuracy, "loss": loss})
+        # update best accuracy 
+        global BEST_ACCURACY
+        if accuracy > BEST_ACCURACY:
+            BEST_ACCURACY = accuracy
+            fl.common.logger.log(
+                INFO, f"eval/loss {loss} accuracy {accuracy} BEST ACCURACY"
+            )
+        wandb.log({"best_accuracy": BEST_ACCURACY})
+
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+
+if __name__ == "__main__":
+    main()
